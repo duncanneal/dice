01d6dfc882f89d3117cdf2dd3b7a32f8b1fe4fda
Duncan Neal <duncan.neal@gmail.com>
2015-05-11 16:38:02 -0400

    Dice Test

--- game.rb
+++ game.rb
@@ -1,9 +1,37 @@
 require "./dice"
 
+
 class Player
+
+  attr_reader :dice
+
+  def initialize(name, dice)
+    @dice = dice
+  end 
 end
 
-
 class Game
-  # Players roll their dice until someone wins
+
+  attr_reader :winner, :turn
+
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+    @turn = 0
+  end
+
+  def play!
+    until @winner
+      player_1_roll = @player_1.dice.roll
+      player_2_roll = @player_2.dice.roll
+
+    @turn += 1    
+
+  if player_1_roll > player_2_roll
+      @winner = @player_1
+  elsif player_1_roll < player_2_roll
+      @winner = @player_2
+      end      
+    end    
+  end   
 end
--- game_test.rb
+++ game_test.rb
@@ -15,14 +15,14 @@ class GameTest < MiniTest::Test
     future = [2,3,6,1]
     dice = PsychicDice.new future
     future.each do |predicted|
-      assert_equal dice.roll, predicted
+      assert_equal predicted,dice.roll
     end
   end
 
   def test_players_have_dice
     dice  = Dice.new
     alice = Player.new "alice", dice
-    assert_equal alice.dice, dice
+    assert_equal dice, alice.dice
   end
 
   def test_somebody_eventually_wins
@@ -40,7 +40,7 @@ class GameTest < MiniTest::Test
     game  = Game.new su, james
     game.play!
 
-    assert_equal game.winner, james
+    assert_equal james, game.winner
   end
 
   def test_predictable_games_are_predictable
@@ -49,7 +49,7 @@ class GameTest < MiniTest::Test
     game = Game.new brit, sarah
     game.play!
 
-    assert_equal game.winner, brit
-    assert_equal game.turn, 3
+    assert_equal brit, game.winner
+    assert_equal 3, game.turn
   end
 end
